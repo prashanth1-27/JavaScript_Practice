1987c66c5f4fe0f5cb4d292e95e92594c07f9f9e
prasanth <your@email.com>
2026-06-06 10:17:52 +0530

    JavaScript

--- Variables & Conditions/Day-1(Inter & Adv).js
+++ Variables & Conditions/Day-1(Inter & Adv).js
@@ -171,3 +171,98 @@ switch(operator){
         console.log("Type Error");
 }
 
+//Hard
+
+//1.Find the largest among 4 numbers.
+let l1 = 10;
+let l2 = 20;
+let l3 = 30 ;
+let l4 = 40;
+
+if(l1>l2 && l1>l3 && l1>l4){
+    console.log("l1 is largest");
+}
+else if(l2>l1 && l2>l3 && l2>l4){
+    console.log("l2 is largest");
+}
+else if(l3>l1 && l3>l2 && l3>l4){
+    console.log("l3 is largest");
+}
+else if(l4>l1 && l4>l2 && l4>l3){
+    console.log("l4 is largest")
+}
+else{
+    console.log("Error");
+}
+
+//2.Check whether a number lies between 50 and 100.
+let nu1 = 77;
+let r = nu1 >= 50 && nu1 <= 100 ? "the number lies between 50 and 100" : "Not lies";
+console.log(r);
+
+//3.Find whether a student passed: Minimum 35 in all subjects.
+let b1 = 3;
+let b2 = 90;
+let b3 = 80;
+if(b1>= 35 && b2 >= 35 && b3 >= 35){
+    console.log("Pass");
+}
+else {
+    console.log("Fail")
+}
+
+/*4.Electricity bill calculator using conditions.
+First 100 units → ₹5/unit
+Next 100 units → ₹7/unit
+Above 200 units → ₹10/unit */
+let unit = 127;
+let bill;
+if(unit <=100){
+    bill = unit * 5;
+}
+else if(unit <=200){
+    bill = unit * 7;
+}
+else{
+    bill = unit * 10;
+}
+console.log("Bill", bill);
+
+//5.Salary calculator: HRA = 20%,DA = 10%
+let base_salary = 20000;
+let hra = 0.02;
+let da = 0.01;
+let total_salary = base_salary + hra + da
+console.log("Total_salary:",total_salary);
+
+//6.ATM Withdrawal validation: Balance available?, Amount multiple of 100?
+let balance = 10000;
+let amount = 4700;
+if( balance >= amount && amount %100 === 0){
+    console.log("Withdrawal Sucessful")
+}
+else{
+    console.log("Withdrawal Unsucessful")
+}
+
+//7.Age category checker: Child, Teen, Adult, Senior Citizen
+let age1 =21 ;
+if(age1 <= 12){
+    console.log("Child")
+}
+else if ( age1 <=20){
+    console.log("Teen")
+}
+else if ( age1 <= 50){
+    console.log("Adult")
+}
+else{
+    console.log("Senior Citizen")
+}
+
+//8.BMI Calculator using: Weight, Height
+
+let weight = 70;
+let height = 1.75;
+let bmi = weight/(height* height);
+console.log("BMI:",bmi.toFixed(2));
